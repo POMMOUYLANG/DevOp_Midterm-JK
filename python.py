631fa6ed15e0c84aa48a1977f3570aa28b82e9ef
POMMOUYLANG <mouylangpom@gamail.com>
2024-03-28 15:08:04 +0700

    DevOps:Midterm

---
+++ python.py
@@ -0,0 +1,15 @@
+import os
+list1 = [8, 5, 6, 1, 10]
+list1.reverse()
+print("The reversed list is", list1)
+
+list2 = [91, 29, 10, 89, 79, 44, 88, 99]
+list2.sort()
+print("The sorted list is", list2)
+
+list3 = []
+list3 = list1.copy()
+print("List3 = ", list3)
+
+indexvalue = list2[2:6]
+print("The index values are", indexvalue)
