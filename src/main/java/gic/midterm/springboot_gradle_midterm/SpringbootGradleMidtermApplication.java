678a957233d2b5109b4b50d1b2b8d0bd9fad5648
POMMOUYLANG <mouylangpom@gamail.com>
2024-03-29 14:16:13 +0700

    DevOps:Midterm

---
+++ src/main/java/gic/midterm/springboot_gradle_midterm/SpringbootGradleMidtermApplication.java
@@ -0,0 +1,13 @@
+package gic.midterm.springboot_gradle_midterm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGradleMidtermApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGradleMidtermApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gic/midterm/springboot_gradle_midterm/SpringbootGradleMidtermApplicationTests.java
@@ -0,0 +1,13 @@
+package gic.midterm.springboot_gradle_midterm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGradleMidtermApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
